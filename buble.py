aefd6a7d8eea4c2b68a24b271f1229a52c273028
Владислав Карпов <116154991+IcRipperr@users.noreply.github.com>
2024-09-19 10:13:27 +0300

    Update buble.py

--- buble.py
+++ buble.py
@@ -1,8 +1,13 @@
-lst =[]
 import random
-for a in range(10):
-    lst.append(random.randint(0, 100))
 
+arr =[]
+vibor = input("Случайные числа?")
+if vibor.capitalize() == "Да":
+    for a in range(9):
+        arr.append((random.randint(0, 100)) + (random.randint(0, 100)) / 100)
+else:
+    for a in range(9):
+        arr.append (int(input("Введите число: ")))
 def bublesort(lst):
     for passesLeft in range(len(lst)-1,0,-1):
         for index in range(passesLeft):
@@ -10,4 +15,4 @@ def bublesort(lst):
                 lst[index], lst[ index + 1] = lst[ index + 1], lst[index]
                 print(lst)
     return lst
-print(bublesort(lst))
+print(bublesort(arr))
